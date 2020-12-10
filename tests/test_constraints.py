badf66bc1c0c3f636bb1eecc949b7d69c6cff34f
Diogo Gomes <dgomes@ua.pt>
2020-12-10 10:52:16 +0000

    generic test

--- tests/test_constraints.py
+++ tests/test_constraints.py
@@ -6,7 +6,20 @@ def test_exercicio1_4():
     assert mapas.cs.search() == {'A': 'red', 'B': 'blue', 'C': 'red', 'D': 'blue', 'E': 'green'}
 
 def test_exercicio1_5():
-    assert amigos.cs.search() == {'Andre': ('Claudio', 'Bernardo'), 'Bernardo': ('Andre', 'Claudio'), 'Claudio': ('Bernardo', 'Andre')}
+    solution = amigos.cs.search()
+
+    for amigo, (bicicleta, chapeu) in solution.items():
+        assert amigo != bicicleta
+        assert amigo != chapeu
+        if chapeu == "Claudio":
+            assert bicicleta == "Bernardo"
+        
+    bicicletas = [ bicicleta for _, (bicicleta, _) in solution.items() ]
+    assert len(bicicletas) == len(set(bicicletas))
+
+    chapeus = [ chapeu for _, (_, chapeu) in solution.items() ]
+    assert len(chapeus) == len(set(chapeus))
+
 
 def test_exercicio2():
     assert amigos.cs.calls == 18
